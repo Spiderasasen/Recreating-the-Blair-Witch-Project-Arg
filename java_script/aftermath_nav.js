3f54dabb7919c78f9dbf1fed0ef348b66f7382ad
spiderasasen <spiderasasen@gmail.com>
2025-11-11 13:18:26 -0500

    Aftermath I did the basic navigation section of the code. I also made the basic layout of the code.

---
+++ java_script/aftermath_nav.js
@@ -0,0 +1,19 @@
+const evidence = document.getElementById("evidence");
+const search = document.getElementById("search");
+const interview = document.getElementById("interview");
+const news = document.getElementById("news");
+
+//navigating to all the first sections of the code
+evidence.addEventListener("click", () => {
+    console.log('going to aftermath');
+    window.location.href = '/aftermath.html';
+});
+search.addEventListener("click", () => {
+    console.log('going to search');
+});
+interview.addEventListener("click", () => {
+    console.log('going to interview');
+});
+news.addEventListener("click", () => {
+    console.log('going to news');
+});
