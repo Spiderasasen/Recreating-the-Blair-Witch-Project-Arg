0c1c2960d6dd4799cd36567c9b3c13bd9489d261
spiderasasen <spiderasasen@gmail.com>
2025-11-18 11:24:53 -0500

    Working on legacy With the help of the last section, i fixed it to be with audio than with video. I love this

--- java_script/legacy_navigation.js
+++ java_script/legacy_navigation.js
@@ -6,3 +6,7 @@ discovered.addEventListener("click", () => {
     console.log("discovered clicked");
     window.location.href = '../../legacy.html';
 });
+audios.addEventListener("click", () => {
+    console.log("audio clicked");
+    window.location.href = '../../Legacy_sections/Audio/audio.html';
+})
